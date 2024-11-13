570efb10b210e0654ee7201dbf9ce4604718a1de
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-11-13 22:26:00 +0700

    Add a headshot image with CSS styling, remove JS, redo the 'about me'

--- script.js
+++
@@ -1,8 +0,0 @@
-function copy (that) {
-    var inp = document.createElement('input');
-    document.body.appendChild(inp);
-    inp.value = that.id;
-    inp.select();
-    document.execCommand('copy', false);
-    inp.remove();
-}
